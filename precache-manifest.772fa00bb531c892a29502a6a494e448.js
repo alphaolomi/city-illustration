848db510ea58206cd8639bb2b5206906a5576a03
Alpha Olomi <alphaolomi@gmail.com>
2019-10-17 06:59:02 +0300

    Updates

---
+++ precache-manifest.772fa00bb531c892a29502a6a494e448.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed8727fbb47eb4818f040e4861240a1",
+    "url": "/city-illustration/index.html"
+  },
+  {
+    "revision": "672db166665badd8c4e2",
+    "url": "/city-illustration/static/css/main.4d11c837.chunk.css"
+  },
+  {
+    "revision": "c1b283239a0e46205461",
+    "url": "/city-illustration/static/js/2.e433c67a.chunk.js"
+  },
+  {
+    "revision": "672db166665badd8c4e2",
+    "url": "/city-illustration/static/js/main.8b607914.chunk.js"
+  },
+  {
+    "revision": "7023c0e498fc5b54ad13",
+    "url": "/city-illustration/static/js/runtime-main.a1f7e3ad.js"
+  }
+]);
